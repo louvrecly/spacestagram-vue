07d19f51bc2049d5052a37dffbebc8e350751711
Louvre Chow <louvrecly@gmail.com>
2022-01-18 17:04:36 -0500

    feat/home-page: Create ImagePost component

---
+++ components/PostsList/ImagePost.vue
@@ -0,0 +1,56 @@
+<template>
+  <li class="post">
+    <div class="wrapper">
+      <img class="image" :src="url" :alt="title">
+    </div>
+
+    <div class="container">
+      <div class="info">
+        <h3 class="title text-large">
+          {{ title }}
+        </h3>
+
+        <p class="date text-normal">{{ date }}</p>
+      </div>
+
+      <p class="explanation">{{ explanation }}</p>
+    </div>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'ImagePost',
+  props: {
+    title: {
+      type: String,
+      default: 'Missing Title'
+    },
+    explanation: {
+      type: String,
+      default: 'Missing Explanation'
+    },
+    url: {
+      type: String,
+      required: true
+    },
+    date: {
+      type: String,
+      default: 'Date Unknown'
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+.post
+  .wrapper
+    .image
+      width: 100%
+
+  .container
+    .info
+      display: flex
+      justify-content: space-between
+      align-items: center
+</style>
