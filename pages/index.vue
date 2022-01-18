6bf91cf22d6bea3c227163336ca2eb3b753fe294
Louvre Chow <louvrecly@gmail.com>
2022-01-18 17:07:59 -0500

    feat/home-page: Import PostsList to home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,32 @@
 <template>
-  <div>Welcome to Spacestagram! &#129680;</div>
+  <div class="home">
+    <section class="container responsive-margins">
+      <h1 class="title text-larger">
+        Welcome to Spacestagram! &#129680;
+      </h1>
+
+      <posts-list :posts="posts"></posts-list>
+    </section>
+  </div>
 </template>
 
 <script>
+import PostsList from '~/components/PostsList'
+
 export default {
   name: 'HomePage',
+  components: { PostsList },
+  data () {
+    return { posts: [] }
+  }
 }
 </script>
+
+<style lang="sass" scoped>
+@import ~assets/css/base/typography
+@import ~assets/css/components/section
+
+.home
+  background-color: black
+  color: gold
+</style>
