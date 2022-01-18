3b13b7196ae718ccc0cd5cf093219f6394518280
Louvre Chow <louvrecly@gmail.com>
2022-01-18 17:05:15 -0500

    feat/home-page: Create PostsList component and import ImagePost to it

---
+++ components/PostsList/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <ul class="posts-list">
+    <image-post
+      v-for="(post, idx) in posts"
+      :key="idx"
+      :title="post.title"
+      :explanation="post.explanation"
+      :url="post.url"
+      :date="post.date"
+    ></image-post>
+  </ul>
+</template>
+
+<script>
+import ImagePost from './ImagePost'
+
+export default {
+  name: 'PostsList',
+  components: { ImagePost },
+  props: {
+    posts: {
+      type: Array,
+      default: () => []
+    }
+  }
+}
+</script>
