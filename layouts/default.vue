0355944e16d99a7d68d598598184872edfb66f97
Louvre Chow <louvrecly@gmail.com>
2022-01-19 15:49:11 -0500

    enh/nav-bar: Create default layout

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="default-layout">
+    <nav-bar></nav-bar>
+
+    <nuxt></nuxt>
+  </div>
+</template>
+
+<script>
+import NavBar from '~/components/NavBar'
+
+export default {
+  name: 'DefaultLayout',
+  components: { NavBar }
+}
+</script>
