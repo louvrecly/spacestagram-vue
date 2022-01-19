0f47429d2c850cd8965ee457396ca3cb280b84ab
Louvre Chow <louvrecly@gmail.com>
2022-01-18 21:18:13 -0500

    feat/store: Adjust styling

--- components/PostsList/ImagePost.vue
+++ components/PostsList/ImagePost.vue
@@ -6,14 +6,12 @@
 
     <div class="container">
       <div class="info">
-        <h3 class="title text-large">
-          {{ title }}
-        </h3>
+        <h3 class="title">{{ title }}</h3>
 
-        <p class="date text-normal">{{ date }}</p>
+        <p class="date">{{ date }}</p>
       </div>
 
-      <p class="explanation">{{ explanation }}</p>
+      <p class="explanation text-small">{{ explanation }}</p>
     </div>
   </li>
 </template>
@@ -43,14 +41,38 @@ export default {
 </script>
 
 <style lang="sass" scoped>
+@import ~assets/css/base/typography
+@import ~assets/css/utils/media
+
 .post
   .wrapper
+    border-radius: 8px
+    overflow: hidden
+
     .image
       width: 100%
 
   .container
     .info
-      display: flex
-      justify-content: space-between
-      align-items: center
+      padding: 20px 0
+
+      @media #{$tablets-up}
+        display: flex
+        justify-content: space-between
+        align-items: center
+
+      .title
+        +font-size-small
+
+        @media #{$tablets-up}
+          +font-size-normal
+
+      .date
+        +font-size-smaller
+
+        @media #{$tablets-up}
+          +font-size-normal
+
+    .explanation
+      color: rgba(gold, 0.7)
 </style>
