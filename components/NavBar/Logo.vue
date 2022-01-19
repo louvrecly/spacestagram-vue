9f54f96dee0592352cac23cd59a403b1b68d8521
Louvre Chow <louvrecly@gmail.com>
2022-01-19 16:08:30 -0500

    enh/nav-bar: Extract AppLogo from NavBar

---
+++ components/NavBar/Logo.vue
@@ -0,0 +1,34 @@
+<template>
+  <h1 class="logo text-bold">
+    <span class="icon">&#129680;</span><span class="title">spacestagram</span>
+  </h1>
+</template>
+
+<script>
+export default {
+  name: 'AppLogo'
+}
+</script>
+
+<style lang="sass" scoped>
+@import ~assets/css/base/typography
+@import ~assets/css/utils/media
+
+.logo
+  color: goldenrod
+  font-family: $font-family-logo
+
+  .icon
+    margin: 0 5px 0 0
+    +font-size-normal
+
+    @media #{$tablets-up}
+      margin: 0 10px 0 0
+      +font-size-large
+
+  .title
+    +font-size-large
+
+    @media #{$tablets-up}
+      +font-size-larger
+</style>
