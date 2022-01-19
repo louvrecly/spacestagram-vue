d86cfcc0d26bbd8915dc30c3b8d664bd631d534e
Louvre Chow <louvrecly@gmail.com>
2022-01-19 15:49:41 -0500

    enh/nav-bar: Update home page to remove duplicated title

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,6 @@
 <template>
   <div class="home">
     <section class="container responsive-margins">
-      <h1 class="title text-bold">
-        <span class="icon">&#129680;</span>
-        <span class="text">spacestagram</span>
-      </h1>
-
       <posts-list :posts="posts"></posts-list>
     </section>
   </div>
@@ -33,37 +28,15 @@ export default {
 </script>
 
 <style lang="sass" scoped>
-@import ~assets/css/base/typography
 @import ~assets/css/utils/media
 @import ~assets/css/components/section
 
 .home
-  background-image: linear-gradient(90deg, #151515 0%, #1D1D1D 10%, #1D1D1D 90%, #151515 100%)
+  background-image: $background-gradient
 
   .container
-    padding: 50px 0
+    padding: 120px 0 100px
 
     @media #{$tablets-up}
-      padding: 100px 0
-
-    .title
-      margin: 0 0 30px
-      color: goldenrod
-      font-family: $font-family-logo
-
-      @media #{$tablets-up}
-        margin: 0 0 50px
-
-      .icon
-        margin: 0 5px 0 0
-        +font-size-normal
-
-        @media #{$tablets-up}
-          +font-size-large
-
-      .text
-        +font-size-large
-
-        @media #{$tablets-up}
-          +font-size-larger
+      padding: 160px 0
 </style>
