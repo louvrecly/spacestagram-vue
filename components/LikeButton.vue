c2c2e3fe59d3b7791e7ea6c1fd049497af198746
Louvre Chow <louvrecly@gmail.com>
2022-01-18 23:23:19 -0500

    feat/like-button: Create LikeButton component

---
+++ components/LikeButton.vue
@@ -0,0 +1,51 @@
+<template>
+  <button class="like-button" :class="{ liked }" @click="toggleLike">
+    <font-awesome-icon :icon="[iconType, 'heart']"></font-awesome-icon>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'LikeButton',
+  props: {
+    liked: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    iconType () {
+      return this.liked ? 'fas' : 'far'
+    }
+  },
+  methods: {
+    toggleLike () {
+      this.$emit('toggleLike', this.liked)
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+@import ~assets/css/base/typography
+@import ~assets/css/utils/media
+
+.like-button
+  padding: 5px
+  background-color: transparent
+  border: none
+  color: silver
+  +font-size-normal
+  line-height: 1em
+  cursor: pointer
+  transition: transform .3s ease
+
+  @media #{$tablets-up}
+    padding: 10px
+
+  &:hover
+    transform: scale(1.2)
+
+  &.liked
+    color: crimson
+</style>
