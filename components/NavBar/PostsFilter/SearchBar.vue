f31f8262b483163facd107ceb2233f9686710d96
Louvre Chow <louvrecly@gmail.com>
2022-01-19 19:09:01 -0500

    feat/filter-modal: Create SearchBar

---
+++ components/NavBar/PostsFilter/SearchBar.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="search-bar">
+    <label class="label" for="search">
+      <font-awesome-icon :icon="['fas', 'search']"></font-awesome-icon>
+    </label>
+
+    <input
+      id="search"
+      class="input"
+      name="search"
+      type="text"
+      :value="value"
+      placeholder="Type something here!"
+      @input="updateValue"
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: String,
+      default: ''
+    }
+  },
+  methods: {
+    updateValue (e) {
+      this.$emit('input', e.target.value)
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+@import ~assets/css/base/typography
+
+.search-bar
+  .label
+    margin: 0 10px 0 0
+
+  .input
+    background-color: transparent
+    border: none
+    color: goldenrod
+    +font-size-small
+    border-radius: 8px
+    outline: none
+</style>
