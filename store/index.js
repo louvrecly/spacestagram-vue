a23ba8d8a79cd9dfc914b2308bfada1046213f93
Louvre Chow <louvrecly@gmail.com>
2022-01-19 23:43:05 -0500

    enh/load-posts: Add startDate state and loadMorePosts action in the store

--- store/index.js
+++ store/index.js
@@ -1,25 +1,36 @@
 export const state = () => ({
-  posts: []
+  posts: [],
+  startDate: null
 })
 
 export const getters = {
-  getPosts: state => state.posts
+  getPosts: state => state.posts,
+  getStartDate: state => state.startDate
 }
 
 export const mutations = {
   SET_POSTS (state, posts) {
     state.posts = posts
+  },
+  SET_START_DATE (state, startDate) {
+    state.startDate = startDate
   }
 }
 
 export const actions = {
-  async loadPosts ({ commit }, startDate) {
+  async loadPosts ({ commit, state }, startDate, endDate) {
     const posts = await this.$axios.$get('/planetary/apod', {
-      params: { api_key: process.env.NASA_API_KEY, start_date: startDate }
+      params: {
+        api_key: process.env.NASA_API_KEY,
+        start_date: startDate,
+        end_date: endDate
+      }
     })
 
-    const sortedPosts = posts.sort((postA, postB) => postA.date < postB.date ? 1 : -1)
+    const sortedNewPosts = posts.sort((postA, postB) => postA.date < postB.date ? 1 : -1)
+    const sortedPosts = [...state.posts, ...sortedNewPosts]
 
+    commit('SET_START_DATE', startDate)
     commit('SET_POSTS', sortedPosts)
     return sortedPosts
   }
